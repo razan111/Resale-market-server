45f3dbb940e1dabb7362d951b82951e8d796ab5a
razan111 <razan15-12966@diu.edu.bd>
2022-11-24 14:09:17 +0600

    server set up

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const app = express()
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', async(req, res) =>{
+    res.send('resale server running')
+})
+
+app.listen(port, () => {
+    console.log(`Resale running on: ${port}`)
+})
