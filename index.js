685c023a1316ba120c0787ca13c25f8df847ce4e
razan111 <razan15-12966@diu.edu.bd>
2022-11-25 18:16:20 +0600

    seller and buyer added in db

--- index.js
+++ index.js
@@ -1,69 +1,95 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
-const jwt = require('jsonwebtoken');
+const express = require('express')
+const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const app = express()
+require('dotenv').config()
 
-//middleware
+const jwt = require('jsonwebtoken');
+
+
 app.use(cors())
 app.use(express.json())
 
 
+
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.llsjgoo.mongodb.net/?retryWrites=true&w=majority`;
 
-// console.log(uri)
-
+console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+function verifyJWT (req, res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send('unauthorized access')
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function(err, decoded){
+        if(err){
+            return res.status(403).send({message: 'forbidden access'})
+        }
+        req.decoded = decoded
+        next();
+    })
+
+}
+
 
 async function run(){
     try{
-        const serviceCollections = client.db('resalestore').collection('services')
 
-        const buyersCollection = client.db('resalestore').collection('buyers')
-
-        app.get('/services', async(req, res) =>{
-            const query = {}
-            const options = await serviceCollections.find(query).toArray()
-            res.send(options)
-        })
-
-        app.post('/buyer', async(req, res) =>{
-            const buyer = req.body;
-            const result = await buyersCollection.insertOne(buyer)
-            res.send(result)
-        })
+        const usersCollection = client.db('resalePortal').collection('users')
 
         app.get('/jwt', async(req, res) =>{
             const email = req.query.email;
             const query = {email: email}
-            const buyer = await buyersCollection.findOne(query)
-
-            if(buyer){
-                const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '1d'})
+            const user = await usersCollection.findOne(query)
+            if(user && user.email){
+                const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '1h'})
                 return res.send({accessToken: token})
             }
+            console.log(user)
             res.status(403).send({accessToken: ''})
+
+        })
+
+        app.get('/users', async(req, res) =>{
+            const query = {};
+            const users = await usersCollection.find(query).toArray()
+            res.send(users)
         })
 
 
+        app.post('/users', async(req, res) =>{
+            const user = req.body;
+            const result = await usersCollection.insertOne(user)
+            res.send(result)
+        })
+
 
     }
+
     finally{
 
     }
-
 }
+
 run().catch(console.log)
 
 
-app.get('/', async(req, res) =>{
-    res.send('resale server running')
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
 app.listen(port, () => {
-    console.log(`Resale running on: ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
